0347b711adef606707d49cc72c503968327e4571
Umed <nabievumed8@gmail.com>
2022-07-19 19:17:59 +0300

    Lesson6Examples

---
+++ src/Lesson6/EchoClientTest.java
@@ -0,0 +1,25 @@
+package Lesson6;
+
+import Lesson6.Connection.EchoClient;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EchoClientTest {
+    @Test
+    public void test() throws IOException {
+        EchoClient client = new EchoClient();
+        client.startConnection("127.0.0.1", 9999);
+        String resp1 = client.sendMessage("hello");
+        String resp2 = client.sendMessage("world");
+        String resp3 = client.sendMessage("!");
+        String resp4 = client.sendMessage(".");
+
+        assertEquals("hello", resp1);
+        assertEquals("world", resp2);
+        assertEquals("!", resp3);
+        assertEquals("good bye", resp4);
+    }
+}
